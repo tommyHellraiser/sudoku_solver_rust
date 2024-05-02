ff48f288e040090afdfecb508a28cdc699e2ebf2
tommyHellraiser <nacho.ponce25@gmail.com>
2024-05-01 23:45:47 -0300

    Clean commit, added gitignore and README

--- target/debug/deps/sudoku_solver-67739ba2e5ca1c2f.d
+++
@@ -1,9 +0,0 @@
-E:\Rust\sudoku_solver\target\debug\deps\libsudoku_solver-67739ba2e5ca1c2f.rmeta: src\lib.rs Cargo.toml
-
-E:\Rust\sudoku_solver\target\debug\deps\sudoku_solver-67739ba2e5ca1c2f.d: src\lib.rs Cargo.toml
-
-src\lib.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
-# env-dep:CLIPPY_CONF_DIR
